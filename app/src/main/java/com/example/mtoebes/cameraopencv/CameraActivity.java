8a5ebbf86837e75f24ab14d38904f6d2667e70dd
mtoebes <mtoebes@cisco.com>
2015-10-19 22:08:18 -0400

    comment CameraActivity

--- app/src/main/java/com/example/mtoebes/cameraopencv/CameraActivity.java
+++ app/src/main/java/com/example/mtoebes/cameraopencv/CameraActivity.java
@@ -9,49 +9,55 @@ import org.opencv.android.CameraBridgeViewBase.CvCameraViewFrame;
 import org.opencv.android.CameraBridgeViewBase.CvCameraViewListener2;
 import org.opencv.core.Mat;
 
-// http://www.jayrambhia.com/blog/beginning-android-opencv/
+/*
+ * CameraActivity opens the back camera and displays the view to the screen.
+ * By calling takePhoto(), the current view is saved to a jpg file in mat format.
+ */
 public class CameraActivity extends Activity implements CvCameraViewListener2 {
-    private static final String TAG = "OCVSample::Activity";
+    private static final String TAG = "CameraActivity";
     private Mat mRgba;
     private CameraBridgeViewBase mOpenCvCameraView;
 
+    // Called as part of the activity lifecycle when this activity is starting.
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_camera);
-        mOpenCvCameraView = (CameraBridgeViewBase) findViewById ( R.id.surface_view );
+        mOpenCvCameraView = (CameraBridgeViewBase)findViewById(R.id.surface_view);
         mOpenCvCameraView.setCvCameraViewListener(this);
     }
 
-    public void takePhoto(View view) {
-        PhotoHelper.saveMat(mRgba);
-    }
-
+    // Called as part of the activity lifecycle when this activity is no longer visible.
     @Override
-    public void onPause()
-    {
+    public void onPause() {
         super.onPause();
         if (mOpenCvCameraView != null)
             mOpenCvCameraView.disableView();
     }
 
+    // Called as part of the activity lifecycle when this activity becomes visible.
     @Override
-    public void onResume()
-    {
+    public void onResume() {
         super.onResume();
         mOpenCvCameraView.enableView();
     }
 
+    // onClick event that is called when the button with id "take_photo_button" is pressed
+    public void takePhoto(View view) {
+        PhotoHelper.saveMat(mRgba);
+    }
+
+    // This method is invoked when camera preview has started.
     @Override
     public void onCameraViewStarted(int width, int height) {
         mRgba = new Mat();
     }
 
+    // This method is invoked when camera preview has been stopped for some reason.
     @Override
-    public void onCameraViewStopped() {
+    public void onCameraViewStopped() { }
 
-    }
-
+    // This method is invoked when delivery of the frame needs to be done.
     @Override
     public Mat onCameraFrame(CvCameraViewFrame inputFrame) {
         mRgba = inputFrame.rgba();
