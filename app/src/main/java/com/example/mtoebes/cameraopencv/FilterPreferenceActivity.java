2c787bbf62c0ddca98a7065add85b5a279eebc8c
mtoebes <mtoebes@cisco.com>
2015-10-25 19:33:05 -0400

    update gradle to use 23.0.0

--- app/src/main/java/com/example/mtoebes/cameraopencv/FilterPreferenceActivity.java
+++ app/src/main/java/com/example/mtoebes/cameraopencv/FilterPreferenceActivity.java
@@ -1,13 +1,7 @@
 package com.example.mtoebes.cameraopencv;
 
 import android.os.Bundle;
-import android.preference.Preference;
 import android.preference.PreferenceActivity;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.View;
 
 public class FilterPreferenceActivity extends PreferenceActivity {
 
