c932e6c99dfcd654b189ce7f20216be8640a4bcb
mtoebes <mtoebes@cisco.com>
2015-10-30 22:13:24 -0400

    add logs to filterMat to see what parameters are used

--- app/src/main/java/com/example/mtoebes/cameraopencv/FilteredMat.java
+++ app/src/main/java/com/example/mtoebes/cameraopencv/FilteredMat.java
@@ -21,10 +21,10 @@ public class FilteredMat {
     private static final String TAG = "FilterMat";
     private static final int DEPTH = CvType.CV_8U;
     private static final double D_RHO = 1;
-    private static final double D_THETA = Math.PI/180;
+    private static final double D_THETA = Math.PI/90;
 
     private static final String
-            orignal_tag = "Original", color_tag = "Color Mask", blur_tag = "Gaussian",
+            orignal_tag = "Original", gray_tag = "Gray Mask", color_tag = "Color Mask", blur_tag = "Gaussian",
             sobel_tag = "Sobel", laplacian_tag = "Laplacian", canny_tag = "Canny", hough_tag = "Hough";
 
     private static int channel_num;
@@ -61,7 +61,9 @@ public class FilteredMat {
             case orignal_tag:
                 return mSrcMat;
             case blur_tag:
-                return getGaussianBlur(get(color_tag), resMat);
+                return getGaussianBlur(get(gray_tag), resMat);
+            case gray_tag:
+                return getGrayScale(mSrcMat, resMat);
             case color_tag:
                 return getChannel(mSrcMat, resMat);
             case sobel_tag:
@@ -79,31 +81,37 @@ public class FilteredMat {
     }
 
     public Mat getGrayScale(Mat srcMat, Mat resMat) {
+        Log.v(TAG, "getGrayScale");
         Imgproc.cvtColor(srcMat, resMat, Imgproc.COLOR_BGRA2GRAY);
         return resMat;
     }
 
     public Mat getGaussianBlur(Mat srcMat, Mat resMat) {
+        Log.v(TAG, "getGaussianBlur gaussian_ksize " + gaussian_ksize);
         Imgproc.GaussianBlur(srcMat, resMat, new Size(gaussian_ksize, gaussian_ksize), 0, 0);
         return resMat;
     }
 
     public Mat getCanny(Mat srcMat, Mat resMat) {
+        Log.v(TAG, "getCanny  canny_lowerThreshold " + canny_lowerThreshold + " canny_upperThreshold " + canny_upperThreshold);
         Imgproc.Canny(srcMat, resMat, canny_lowerThreshold, canny_upperThreshold);
         return resMat;
     }
 
     public Mat getSobel(Mat srcMat, Mat resMat) {
+        Log.v(TAG, "getSobel sobel_dx " + sobel_dx + " sobel_dy " + sobel_dy + " sobel_ksize " + sobel_ksize);
         Imgproc.Sobel(srcMat, resMat, DEPTH, sobel_dx, sobel_dy, sobel_ksize, 1, 0);
         return resMat;
     }
 
     public Mat getLaplacian(Mat srcMat, Mat resMat) {
+        Log.v(TAG, "getLaplacian laplacian_ksize " + laplacian_ksize);
         Imgproc.Laplacian(srcMat, resMat, DEPTH, laplacian_ksize, 1, 0);
         return resMat;
     }
 
     public Mat getChannel(Mat srcMat, Mat resMat) {
+        Log.v(TAG, "getChannel channel_num " + channel_num);
         if(channel_num < 3) {
             List<Mat> channels = new ArrayList<>(3);
             Core.split(srcMat, channels);
@@ -115,11 +123,14 @@ public class FilteredMat {
     }
 
     public Mat getHoughMat(Mat srcMat, Mat resMat) {
+        Log.v(TAG, "getHoughMat hough_threshold " + hough_threshold + " hough_minLinLength " + hough_minLinLength + " hough_maxLineGap " + hough_maxLineGap);
+        Log.v(TAG,hough_mode);
         Imgproc.cvtColor(srcMat, resMat, Imgproc.COLOR_GRAY2BGR);
 
         Mat lines = new Mat();
         Imgproc.HoughLinesP(srcMat, lines, D_RHO, D_THETA, hough_threshold, hough_minLinLength, hough_maxLineGap);
 
+        Log.v(TAG, "getHoughMat found " + lines.rows() + " lines");
         for(int index = 0; index < lines.rows(); index++) {
             double[] vec = lines.get(index, 0);
             double x1 = vec[0], y1 = vec[1], x2 = vec[2], y2 = vec[3];
@@ -142,8 +153,8 @@ public class FilteredMat {
         canny_lowerThreshold = Integer.parseInt(SP.getString("canny_lower_thresh_pref", "100"));
         canny_upperThreshold = Integer.parseInt(SP.getString("canny_upper_thresh_pref", "200"));
         hough_threshold = Integer.parseInt(SP.getString("hough_threshold_pref", "80"));
-        hough_minLinLength = Integer.parseInt(SP.getString("hough_max_gap_pref", "10"));
-        hough_maxLineGap = Integer.parseInt(SP.getString("hough_min_len_pref", "30"));
-        hough_mode = SP.getString("hough_detection_mode_pref", "Canny");
+        hough_minLinLength = Integer.parseInt(SP.getString("hough_min_len_pref", "10"));
+        hough_maxLineGap = Integer.parseInt(SP.getString("hough_max_gap_pref", "30"));
+        hough_mode = SP.getString("hough_detection_mode_pref", canny_tag);
     }
 }
